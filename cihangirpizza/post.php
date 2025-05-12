ea97a8f5cf6ec7a9875e4c0c20d53030c8a38a1a
feyzaelgun <elgunfeyzanur@gmail.com>
2025-05-12 03:59:35 +0300

    cihangir pizza
    pizza satış websitesi

---
+++ cihangirpizza/post.php
@@ -0,0 +1,13 @@
+<?php
+$isim = $_POST['isim'];
+$soyad = $_POST['soyad'];
+$tel = $_POST['tel'];
+$cins = $_POST['cins'];
+$mesaj = $_POST['mesaj'];
+
+echo "Ad: " . $isim . "<br>";
+echo "Soyad: " . $soyad . "<br>";
+echo "Telefon: " . $tel . "<br>";
+echo "Cinsiyet: " . $cins . "<br>";
+echo "Sipariş: " . $mesaj . "<br>";
+?>
